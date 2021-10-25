e30841db90dcf1989e876a657d59de25f9879ea4
Хайрбек Мамбетов <mnocard@gmail.com>
2021-10-25 22:28:04 +0500

    Связка представления с модель-представлением

---
+++ reactor-crwUI/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using reactor_crwUI.Core;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reactor_crwUI.ViewModel
+{
+    class MainWindowViewModel : ViewModelCore
+    {
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            #endregion
+        }
+
+        #region Свойста
+
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title = "Test app";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
