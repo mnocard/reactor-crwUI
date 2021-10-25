817e47ed1d0dc8dac5c0f1d80024793d21fb3970
Хайрбек Мамбетов <mnocard@gmail.com>
2021-10-25 22:08:03 +0500

    Добавьте файлы проекта.

---
+++ reactor-crwUI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace reactor_crwUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
