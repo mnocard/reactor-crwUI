cb73cd7e38e97363e4042113b1a619796a25023e
Хайрбек Мамбетов <mnocard@gmail.com>
2021-10-26 21:48:30 +0500

    Соответствующие кнопкам команды и свойства

--- reactor-crwUI/ViewModel/MainWindowViewModel.cs
+++ reactor-crwUI/ViewModel/MainWindowViewModel.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.Win32;
-
+﻿using System.Windows.Forms;
 using reactor_crwUI.Core;
 
-using System;
-using System.IO;
 using System.Windows.Input;
 
 namespace reactor_crwUI.ViewModel
@@ -14,6 +11,7 @@ namespace reactor_crwUI.ViewModel
         {
             #region Команды
             RCRWPathCommand = new LambdaCommand(OnRCRWPathCommandExecuted, CanRCRWPathCommandExecute);
+            OutputPathCommand = new LambdaCommand(OnOutputPathCommandExecuted, CanOutputPathCommandExecute);
 
             #endregion
         }
@@ -34,6 +32,60 @@ namespace reactor_crwUI.ViewModel
 
         #endregion
 
+        #region RCRWPath : string - Полное расположение reactor-crw
+
+        /// <summary>Полное расположение reactor-crw</summary>
+        private string _RCRWPath;
+
+        /// <summary>Полное расположение reactor-crw</summary>
+        public string RCRWPath
+        {
+            get => _RCRWPath;
+            set => Set(ref _RCRWPath, value);
+        }
+
+        #endregion
+
+        #region CookiesAccepted : bool - Использовать печеньки
+
+        /// <summary>Использовать печеньки</summary>
+        private bool _CookiesAccepted = false;
+
+        /// <summary>Использовать печеньки</summary>
+        public bool CookiesAccepted
+        {
+            get => _CookiesAccepted;
+            set => Set(ref _CookiesAccepted, value);
+        }
+
+        #endregion
+
+        #region CookiesData : string - Собственно печенька
+
+        /// <summary>Собственно печенька</summary>
+        private string _CookiesData;
+
+        /// <summary>Собственно печенька</summary>
+        public string CookiesData
+        {
+            get => _CookiesData;
+            set => Set(ref _CookiesData, value);
+        }
+
+        #endregion
+
+        #region OutputPath : string - Папка, в которую будет загружен скачанные контент
+
+        /// <summary>Папка, в которую будет загружен скачанные контент</summary>
+        private string _OutputPath;
+
+        /// <summary>Папка, в которую будет загружен скачанные контент</summary>
+        public string OutputPath
+        {
+            get => _OutputPath;
+            set => Set(ref _OutputPath, value);
+        }
+        #endregion
         #endregion
 
         #region Команды
@@ -44,20 +96,44 @@ namespace reactor_crwUI.ViewModel
         /// <summary>Указание расположения reactor-crw</summary>
         private void OnRCRWPathCommandExecuted(object parameter)
         {
-            var dlg = new OpenFileDialog();
+            var dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.DefaultExt = defExt;
             dlg.Filter = dlgFilter;
             var result = dlg.ShowDialog();
 
             if (result == true)
             {
-                Title = dlg.FileName;
+                RCRWPath = dlg.FileName;
             }
         }
 
         private bool CanRCRWPathCommandExecute(object parameter) => true;
 
         #endregion
+
+        #region Указание расположения загрузки контента
+        /// <summary>Указание расположения загрузки контента</summary>
+        public ICommand OutputPathCommand { get; }
+        /// <summary>Указание расположения загрузки контента</summary>
+        private void OnOutputPathCommandExecuted(object parameter)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Выберите папку, в которую будет загружен контент";
+                dialog.ShowNewFolderButton = true;
+                dialog.AutoUpgradeEnabled = true;
+                dialog.UseDescriptionForTitle = true;
+                var result = dialog.ShowDialog();
+                if(result is DialogResult.OK)
+                {
+                    OutputPath = dialog.SelectedPath;
+                }
+            }
+        }
+        private bool CanOutputPathCommandExecute(object parameter) => true;
+
+        #endregion
+
         #endregion
 
         #region Константы
