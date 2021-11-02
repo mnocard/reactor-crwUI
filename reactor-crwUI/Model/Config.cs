89badff730aeb8fc3bf44b4164fcc867e23fac97
Хайрбек Мамбетов <mnocard@gmail.com>
2021-11-02 23:51:44 +0500

    Модель конфигурационного файла

---
+++ reactor-crwUI/Model/Config.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace reactor_crwUI.Model
+{
+    public class Config
+    {
+        public string RCRWPath { get; set; }
+        public bool CookiesAccepted { get; set; }
+        public string CookiesData { get; set; }
+        public string OutputPath { get; set; }
+        public string URL { get; set; }
+        public Dictionary<string, bool> ImageTypes { get; set; }
+        public bool OnePage { get; set; }
+        public int NumOfWorkers { get; set; }
+    }
+}
